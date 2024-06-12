b277f4231b78e2adb4758f16d5dcc1f0c5b6e759
rogeriosaraceni <rsaraceni@gmail.com>
2024-06-12 19:48:04 -0300

    update: js formsSubmitSpinnerToasts

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,21 +2,45 @@
 
 /** ////////////////////////////////////////////////////////////////////
  *
- * - currentYear
- * - DOMPurif protect xss
- * - enable tooltips Bootstrap
+ * - activeTooltips
+ * - mainAppMarginTop
+ * - showCurrentYear
+ * - navigationActive
+ * - initializeSpinnerButtons
+ * - showToasts
  * - fancybox
  * - floatThead
- * - mainAppMarginTop
- * - navigationActive
- * - spinnerBtns
+ * - DOMPurif protect xss
  *
  * - Active Functions
  *
 --------------------------------------------------------------------- */
 
 /** --------------------------------------------------------------------
- * currentYear
+ * activeTooltips
+--------------------------------------------------------------------- */
+function activeTooltips() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
+
+/** --------------------------------------------------------------------
+ * mainAppMarginTop
+--------------------------------------------------------------------- */
+const mainAppMarginTop = () => {
+    const header = document.querySelector('[data-el="top-fixed"]')
+    const mainApp = document.querySelector('[data-el="main-app"]')
+
+    if(header && mainApp){
+        const headerHeight = header.clientHeight + 'px'
+        if(mainApp){
+            mainApp.style.marginTop = headerHeight
+        }
+    }
+}
+
+/** --------------------------------------------------------------------
+ * showCurrentYear
 --------------------------------------------------------------------- */
 const showCurrentYear = () => {
     const currentYear = new Date().getFullYear();
@@ -29,23 +53,72 @@ const showCurrentYear = () => {
 }
 
 /** --------------------------------------------------------------------
- * DOMPurif protect xss
+ * navigationActive
 --------------------------------------------------------------------- */
-const sanitize = (string) => DOMPurify.sanitize(string);
+const navigationActive = () => {
+    const currentUrl = window.location.pathname;
+    const navLinks = document.querySelectorAll(`.navbar-nav .nav-item a[href$="${currentUrl}"]`)
 
-let inputsApp = document.querySelectorAll(".form-control");
-inputsApp.forEach((item) => {
-    item.addEventListener("change", (e) => {
-        const result = sanitize(e.target.value);
-        console.log(result);
+    navLinks.forEach(function (link) {
+        link.classList.add("active")
+
+        const dropdownParent = link.closest(".nav-item.dropdown")
+        dropdownParent?.classList.add("active")
     });
-});
+}
 
 /** --------------------------------------------------------------------
- * enable tooltips Bootstrap
+ * formsSubmitSpinnerToasts
 --------------------------------------------------------------------- */
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+function formsSubmitSpinnerToasts() {
+    const forms = document.querySelectorAll('form[data-form="spinner-toast"]')
+
+    for (const form of forms) {
+        // Remove event listeners duplicados
+        form.removeEventListener('submit', handleSpinnerButtonToast)
+        // Adiciona o event listener
+        form.addEventListener('submit', handleSpinnerButtonToast)
+    }
+}
+function handleSpinnerButtonToast(event) {
+    event.preventDefault()
+
+    const btn = event.submitter
+    btn.classList.add('btn-spinner-disabled');
+    btn.querySelector('.btn-spinner').classList.remove('d-none')
+
+    setTimeout(() => {
+        btn.classList.remove('btn-spinner-disabled');
+        btn.querySelector('.btn-spinner').classList.add('d-none')
+
+        handleToastsDisplay(btn)
+        closeModalIfInside(btn)
+    }, 3000);
+}
+function handleToastsDisplay(btn) {
+    const target = btn.getAttribute('data-toast-target')
+    const toastElement = document.querySelector(`[data-toast-type="${target}"]`)
+
+    if (toastElement) {
+        const toast = new bootstrap.Toast(toastElement)
+        toast.show()
+    }
+}
+// Fecha o modal se o botão estiver dentro de um modal
+function closeModalIfInside(btn) {
+    if (btn.getAttribute('data-modal') !== 'close') {
+        return;
+    }
+
+    const form = btn.closest('form')
+    const modal = form.closest('.modal')
+    if (modal) {
+        const modalInstance = bootstrap.Modal.getInstance(modal)
+        if (modalInstance) {
+            modalInstance.hide()
+        }
+    }
+}
 
 /** --------------------------------------------------------------------
  * fancybox
@@ -73,44 +146,24 @@ if (floatTheadElement) {
 }
 
 /** --------------------------------------------------------------------
- * mainAppMarginTop
+ * DOMPurif protect xss
 --------------------------------------------------------------------- */
-const mainAppMarginTop = () => {
-    const header = document.querySelector('[data-el="top-fixed"]')
-    const mainApp = document.querySelector('[data-el="main-app"]')
+const sanitize = (string) => DOMPurify.sanitize(string);
 
-    if(header){
-        const headerHeight = header.clientHeight + 'px'
-        if(mainApp){
-            mainApp.style.marginTop = headerHeight
-        }
-    }
-}
-
-/** --------------------------------------------------------------------
- * navigationActive
---------------------------------------------------------------------- */
-const navigationActive = () => {
-    const currentUrl = window.location.pathname;
-    const navLinks = document.querySelectorAll(`.navbar-nav .nav-item a[href$="${currentUrl}"]`)
-
-    navLinks.forEach(function (link) {
-        link.classList.add("active")
-
-        const dropdownParent = link.closest(".nav-item.dropdown")
-        dropdownParent?.classList.add("active")
+let inputsApp = document.querySelectorAll(".form-control");
+inputsApp.forEach((item) => {
+    item.addEventListener("change", (e) => {
+        const result = sanitize(e.target.value);
+        console.log(result);
     });
-}
-
-
-/** --------------------------------------------------------------------
- * spinnerBtns
---------------------------------------------------------------------- */
+});
 
 
 /** --------------------------------------------------------------------
  * Active Functions
 --------------------------------------------------------------------- */
-//mainAppMarginTop()
+activeTooltips()
+mainAppMarginTop()
 showCurrentYear()
 navigationActive()
+formsSubmitSpinnerToasts()
---
+++ src/pages/etapas/execucao_comercial/etapas/_script-tabs-rules.js
@@ -0,0 +1,47 @@
+/** -------------------------------------------------------------------
+ * Tabs
+--------------------------------------------------------------------- */
+$('a[data-bs-toggle="tab"]').on('click', function (e) {
+    e.preventDefault()
+
+    const url = $(this).attr("href")
+    const target = $(this).data("bs-target")
+
+    // Verifica se a aba já foi carregada
+    if (!$(target).hasClass('show')) {
+        $(target).load(url, function() {
+            $(this).tab('show')
+
+            //--Inicializa functions quando a tab carrega no mode click
+            $('.selectpicker').selectpicker()
+            activeTooltips()
+            formsSubmitSpinnerToasts()
+        });
+    }
+    else {
+        // Mostra a aba se já estiver carregada
+        $(this).tab('show')
+    }
+
+    //https://datatables.net/examples/api/tabs_and_scrolling.html
+    $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust()
+
+    //https://www.amcharts.com/docs/v4/tutorials/chart-was-not-disposed/
+    //am4core.options.autoDispose = true
+});
+
+$('a[data-bs-toggle="tab"]').on('show.bs.tab', function () {
+    //--Inicializa functions quando a tab vem ativada por default no mode show
+    //$('.selectpicker').selectpicker()
+    //activeTooltips()
+    //formsSubmitSpinnerToasts()
+})
+
+// Carrega e mostra a aba inicial
+// const initialTagert = "#pageSwot" //só alterar aqui
+// const initialTab    = $(`[data-bs-target="${initialTagert}"]`)
+// const initialUrl    = initialTab.attr("href");
+
+// $(initialTagert).load(initialUrl, function() {
+//     initialTab.tab('show')
+// })
