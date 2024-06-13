f9f6209ca0671d9b5a984ad24ad0a179ffb64802
rogeriosaraceni <rsaraceni@gmail.com>
2024-06-13 11:07:39 -0300

    create: função todos checkbox config

--- src/pages/etapas/execucao_comercial/etapas/modal/config/_modal-rules.js
+++ src/pages/etapas/execucao_comercial/etapas/modal/config/_modal-rules.js
@@ -1,17 +1,21 @@
-const form = document.getElementById('formEtapas')
-let checkboxes = document.querySelectorAll('[data-checkbox="etapas"]')
+const form = document.getElementById('formEtapas');
+const selectAllCheckbox = document.getElementById('etapas_All');
+let checkboxes = document.querySelectorAll('[data-checkbox="etapas"]');
+
 
 function verify() {
-    return [].filter.call(checkboxes, (checkbox) => {
-        return checkbox.checked
-    }).length
+    return [].filter.call(checkboxes, (checkbox) => checkbox.checked).length;
+}
+
+function updateSelectAllCheckbox() {
+    const allChecked = [].every.call(checkboxes, (checkbox) => checkbox.checked);
+    selectAllCheckbox.checked = allChecked;
+    localStorage.setItem('etapas_All', allChecked);
 }
 
 function addLocalStorage() {
-    // document.querySelector('#etapa_swot').setAttribute('checked', 'true');
-    // document.querySelector('#etapa_swot').setAttribute('disabled', 'true');
-
-    const checkboxes = [
+    const checkboxIds = [
+        "etapas_All",
         "etapa_swot",
         "etapa_equipe",
         "etapa_treinamentos",
@@ -30,26 +34,37 @@ function addLocalStorage() {
     ];
 
     function handleCheckboxChange(e) {
-        localStorage.setItem(e.target.id, e.target.checked)
+        localStorage.setItem(e.target.id, e.target.checked);
+        updateSelectAllCheckbox();
     }
 
-    checkboxes.forEach(id => {
-        const checkbox = document.getElementById(id)
-
-        if(checkbox){
-            checkbox.addEventListener('change', handleCheckboxChange)
-            checkbox.checked = localStorage.getItem(id) === "true"
+    checkboxIds.forEach(id => {
+        const checkbox = document.getElementById(id);
+        if (checkbox) {
+            checkbox.addEventListener('change', handleCheckboxChange);
+            checkbox.checked = localStorage.getItem(id) === "true";
         }
-    })
+    });
+
+    // Update the selectAllCheckbox state on initial load
+    updateSelectAllCheckbox();
 }
 
-addLocalStorage()
+function handleSelectAllChange() {
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = selectAllCheckbox.checked;
+        localStorage.setItem(checkbox.id, selectAllCheckbox.checked);
+    });
+}
+
+selectAllCheckbox.addEventListener('change', handleSelectAllChange);
+
+addLocalStorage();
 
 form.addEventListener('submit', function(event) {
     const valid = verify();
-
     if (!valid) {
-        event.preventDefault()
-        alert('Selecione ao menos uma opção das Etapas!')
+        event.preventDefault();
+        alert('Selecione ao menos uma opção das Etapas!');
     }
-})
+});
